7f1b2f90e020d512ca2d0159d715575f65af72ba
rhuan.firmino <rhuan.firmino@sinqia.com.br>
2024-02-10 20:06:46 -0300

    articles.py | Criando um crawler com o framework scrapy + correção no article.py

---
+++ scrapy/wikiSpider/wikiSpider/spiders/articles.py
@@ -0,0 +1,21 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+class ArticleSpider(CrawlSpider):
+    name = 'articles'
+    allowed_domains = ['wikipedia.org']
+    start_urls = ['http://en.wikipedia.org/wiki/'
+                  'Benevolent_dictator_for_life']
+    rules = [Rule((allow=r'.*'), callback='parse_items',
+                  follow=True)]
+    
+    def parse_items(self, response):
+        url = response.url
+        title = response.css('h1::text').extract_first()
+        text = response.xpath('//div[@id="mw-content-text"]//text()').extract()
+        lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
+        lastUpdated = lastUpdated.replace('This page was edited on ', '')
+        print('URL is: {}'.format(url))
+        print('TITLE is: {}'.format(title))
+        print('TEXT is: {}'.format(text))
+        print('LAST UPDATED: {}'.format(lastUpdated))LinkExtractor
